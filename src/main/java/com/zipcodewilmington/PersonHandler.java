79f01ad15a143e4f1300ffca18b3aee729259355
'nortonlyr' <'nortonlyr@gmail.com'>
2021-01-14 14:52:20 -0500

    Coding format updated

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -4,54 +4,49 @@ package com.zipcodewilmington;
  * Edited by Norton on 1/14/21.
  */
 public class PersonHandler {
-	
+
 	private final Person[] personArray;
 
-    public PersonHandler(Person[] personArray) {
-        this.personArray = personArray;
-    }
+	public PersonHandler(Person[] personArray) {
+		this.personArray = personArray;
+	}
 
-    public String whileLoop() {
-    	
-        String result = "";
-        int count = 0;
+	public String whileLoop() {
 
-        while (count < personArray.length) {
-           result += personArray[count];
-            count++;
-        }
-        
-        return result;
-    }
+		String result = "";
+		int count = 0;
 
+		while (count < personArray.length) {
+			result += personArray[count];
+			count++;
+		}
+		return result;
+	}
 
-    public String forLoop() {
-        String result = "";
+	public String forLoop() {
+		String result = "";
 
-        StringBuilder names = new StringBuilder();
-        int counter = 0;
+		StringBuilder names = new StringBuilder();
+		int counter = 0;
 
-        for (int i = 0; i < personArray.length; i++) {
+		for (int i = 0; i < personArray.length; i++) {
+			names.append(personArray[counter]);
+			result += personArray[i];
+		}
 
-            names.append(personArray[counter]);
-            result += personArray[i];
-        }
+		return result;
+	}
 
-        return result;
-    }
+	public String forEachLoop() {
+		String result = "";
+		for (Person p : personArray) {
+			result += p.toString();
+		}
+		return result;
+	}
 
+	public Person[] getPersonArray() {
 
-    public String forEachLoop() {
-        String result = "";
-        for (Person p: personArray) {
-        	result += p.toString();
-    }
-        return result;
-    }
-   
-    
-    public Person[] getPersonArray() {
-
-            return personArray;
-        }
-    }
+		return personArray;
+	}
+}
